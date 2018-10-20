b5b5595bce5df859aeeb7f68cb81afcd3990d9df
emilson5 <emilchunchul@gmail.com>
2018-10-20 17:05:47 +0100

    test

--- test.c
+++ test.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-    int a = 4;
+    int a = 5;
     int *my_array = (int *) malloc(5 * sizeof(int));
     int i;
     for (i = 0; i < 10000; i++) {    my_array[i] = i;
