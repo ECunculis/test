0d4720999e7ed13717e0acbd97a89c57cb599b6f
Emils Cunculis <emilchunchul@gmail.com>
2019-06-20 16:53:27 +0300

    Improved indentation

--- test.c
+++ test.c
@@ -5,7 +5,8 @@ int main(int argc, char *argv[]) {
     int a = 5;
     int *my_array = (int *) malloc(5 * sizeof(int));
     int i;
-    for (i = 0; i < 10000; i++) {    my_array[i] = i;
+    for (i = 0; i < 10000; i++) {    
+        my_array[i] = i;
     }
     free(my_array);
 }
